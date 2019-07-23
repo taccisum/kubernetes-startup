011e7d0e1e1506ac025f9e64222a40aeaaa8c2e0
taccisum <liaojinfeng@deepexi.com>
2019-07-23 16:36:46 +0800

    feat: provide startup scrtpt of worker node

---
+++ worker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# MASTER_API_SERVER | ip:port | 192.168.0.207:6443
+# TOKEN | string | sjnl0w.j0tltfmnr62rfzzw
+# CA_CERT_HASH | string | sha256:2f95521879a56654a3658d120d5d75d70edd1b043eb105bf7c3731ef467eadb6
+
+cd `dirname $0`
+
+source ./log.sh
+source ./check.sh
+
+h1 '准备启动kubernetes worker节点'
+
+info '执行检查操作'
+
+check_docker_ce
+if [ $? -eq 1 ];then
+    info '准备安装docker'
+    VERSION=$DOCKER_VERSION . ./prepare/docker.sh
+fi
+
+check_kubenetes
+if [ $? -eq 1 ];then
+    info '准备安装kubenetes'
+    VERSION=$KUBE_VERSION . ./prepare/kubernetes.sh
+fi
+
+info '准备加入kubernetes集群，目标master：'$MASTER_API_SERVER
+kubeadm join $MASTER_API_SERVER \
+    --token $TOKEN \
+    --discovery-token-ca-cert-hash $CA_CERT_HASH
+
+if [ $? -eq 0 ];then
+    success '加入集群成功'
+else
+    error '加入集群失败'
+    exit 1
+fi
